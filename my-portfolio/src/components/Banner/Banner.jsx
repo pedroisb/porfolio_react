c1f3166b48996c47cb328bab790748fdc957f323
Pedro Brito <pedroisb28@gmail.com>
2021-11-16 20:32:41 -0300

    quebrando a cabeça com animação do texto do banner

--- my-portfolio/src/components/Banner/Banner.jsx
+++ my-portfolio/src/components/Banner/Banner.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React from 'react'
 import '../../assets/rpgui/rpgui.css'
 import './Banner.css'
 
@@ -6,7 +6,21 @@ export default () => {
     return(
 
         <div id="banner">
-            <p>SEJA BEM-VINDX AO MEU GAMEFOLIO!</p>
+
+            <div id='boas-vindas'>
+
+                <h1>SEJA BEM-VINDX AO</h1>
+                <h1>MEU GAMEFOLIO!</h1>
+                <h3>CLIQUE NO ÍCONE DO MENU</h3>
+                <h3>E SELECIONE UMA OPÇÃO:</h3>
+            </div>
+
+                <div id='opcoes'>
+                    <p>BIO -</p>
+                    <p>SKILLS -</p>
+                    <p>QUESTS - </p>
+                    <p>INVITE TO PARTY - </p>
+                </div>
         </div>
     )
 }
